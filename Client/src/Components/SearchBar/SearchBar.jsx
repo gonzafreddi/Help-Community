37719f74d2bd1a6cb7a2663bfc106f7e91b7bba3
CLiotta <camilo.liotta999@gmail.com>
2023-09-28 19:22:29 -0300

    Cambio de fuente y fix searchbar

--- Client/src/Components/SearchBar/SearchBar.jsx
+++ Client/src/Components/SearchBar/SearchBar.jsx
@@ -6,9 +6,13 @@ import './SearchBar.css';
 
 export default function SearchBar() {
 
-    const campaigns = datosONG.map((ong) => ( ong.campañas.map( (campaña) => campaña ) ));
+    const data = datosONG.map((ong) => ( ong.campañas.map( (campaña) => campaña ) ));
 
-    console.log(campaigns);
+    const campaigns = [];
+
+    data.map((array) => array.map( (campaña) => campaigns.push(campaña) ));
+
+    // console.log(campaigns);
 
     //TODO --- Manejo de busqueda
     
@@ -24,9 +28,9 @@ export default function SearchBar() {
     
         setSelectedCampaign(campaign);
     
-        navigate(`/details/${selectedCampaign.id}`);
+        navigate(`/details/${selectedCampaign.id}`);                             //todo      Descomentar cuando hay detail
     
-    }
+    } 
 
     const handleInputChange = (event) => {
         
@@ -91,7 +95,16 @@ export default function SearchBar() {
                         placeholder='Busca una campaña'
                     />
 
-                    <button className='search-button' onClick={handleSearchButton} disabled={suggestions.length === 0} >Q</button>
+                    <button className='search-button' onClick={handleSearchButton} disabled={suggestions.length === 0} >
+                        {/* <svg className="lupa" xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <circle cx="5" cy="5" r="4"></circle>
+                            <line x1="11" y1="11" x2="7.7" y2="7.65"></line>
+                        </svg> */}
+                        <i className="material-icons">
+                            search
+                        </i>
+
+                    </button>
 
                     <ul className={`suggestion-list ${showSuggestions ? 'showSuggestions' : ''}`}>
                         {suggestions.map((campaign, index) => (
