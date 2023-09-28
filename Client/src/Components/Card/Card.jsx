59b3fab08799dd364a87a5f24f158ed1d1df9970
LCCaruso <lucas.caruso27@gmail.com>
2023-09-28 12:14:48 -0300

    modificaciones de estilo en Card

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -9,7 +9,7 @@ export const Card = (props) => {
     <div className={style.contenedor}>
         <div className={style.contenedorImagen}>
           <p className={style.ong}>{nombreOng}</p>
-          <img className={style.imagen} src={imagen} alt={nombre} />
+          <img className={style.imagen} src="https://img.freepik.com/foto-gratis/usted-puede-ayudar-dar-concepto-donacion-bienestar_53876-121039.jpg?w=2000" alt={nombre} />
         </div>
       <p className={style.nombre}>{nombre}</p>
       <p className={style.descripcion}>{descripcion}</p>
