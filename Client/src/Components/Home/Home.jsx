517c6f77ddddd0d330562f7044e4ec1115023fcd
ivan <ivanmantello@gmail.com>
2023-09-28 21:28:24 -0300

    paginado- todavía falta solucionar que las ONGs agrupan campañas y eso se toma como una card en el paginado

--- Client/src/Components/Home/Home.jsx
+++ Client/src/Components/Home/Home.jsx
@@ -1,55 +1,52 @@
 import React, { useEffect, useState } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
-// import Pagination from '../Pagination/Pagination';
+import Pagination from '../Pagination/Pagination';
 import { Nav } from "../Nav/Nav";
 import { Cards } from "../Cards/Cards";
+import datosONG from "../../../../Api/ong/ong";
 
 export const Home = () => {
 
-  // -------PAGINADO
-  // const dispatch = useDispatch();
+ // -------PAGINADO
 
-  // useEffect(() => {
-  //     dispatch(getCampaigns());
-  //  //     dispatch(getCategory())   
-  // }, [dispatch]);
+ const [page, setPage] = useState(1);
 
-  //  // const categories = useSelector(state => state.category);
+ // Número de tarjetas por página
+ const cardsPerPage = 8;
+ const totalItems = datosONG.length; 
 
-  // const allCampaigns = useSelector((state) => state.campaigns);
-  // const [page, setPage] = useState(1);
+ // Función para obtener las tarjetas en la página actual
 
-  // // Número de tarjetas por página
-  // const cardsPerPage = 9;
+ const getCurrentPageCampaigns = () => {
+   const startIndex = (page - 1) * cardsPerPage;
+   const endIndex = startIndex + cardsPerPage;
+   const displayedData = datosONG.slice(startIndex, endIndex);
 
-  // // Función para obtener las tarjetas en la página actual
-  // const getCurrentPageCampaigns = () => {
-  //     const startIndex = (page - 1) * cardsPerPage;
-  //     const endIndex = startIndex + cardsPerPage;
-  //     console.log(`startIndex: ${startIndex}, endIndex: ${endIndex}`);
-  //     return allCampaigns.slice(startIndex, endIndex);
-  // };
-  //-----
+   return displayedData;
+
+ };
+ 
+ //-----
 
 
 
 
 
-  return (
-    <div>
-        <Nav/>
+ return (
+   <div>
+       <Nav/>
 
-        <Cards/>
-        
-        {/* // <Pagination
-        //       page={page}
-        //       setPage={setPage}
-        //       itemsPerPage={cardsPerPage}
-        //       totalItems={allCampaigns.length}
-        // />    */}
+       <Cards data={getCurrentPageCampaigns()}/>
+       
+       <Pagination
+           page={page}
+           setPage={setPage}
+           itemsPerPage={cardsPerPage}
+           totalItems={totalItems}
+        /> 
 
-    </div>
-  )
+   </div>
+ )
 }
 
 // {/* <Cards campaigns={getCurrentPageCampaigns()} /> */}
