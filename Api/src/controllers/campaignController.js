4bd56577471064585588957eb01cc1ffaa826e54
JuanPGalli <jpgallir@gmail.com>
2023-09-28 23:49:52 -0300

    limpieza comentarios

--- Api/src/controllers/campaignController.js
+++ Api/src/controllers/campaignController.js
@@ -1,22 +1,17 @@
 const axios = require("axios");
 const { Campaign, Category, Donation, Ong_donor, State } = require("../db");
 const { Op } = require("sequelize");
-/* const {
-  cleanArrayApi,
-  cleanArrayDB,
-  stringAllTeams,
-} = require("../helpers/helpers"); */
+
 const ong = require("../../dataApi/ong");
 
 const getAllCampaign = async function () {
   const campaign = await Campaign.findAll();
-  console.log(campaign);
   if (campaign.length) {
     /* const result = await Campaign.findAll({
       include: {
         model: Ong_donor,
         attributes: ["StateId"],
-        through: { attributes: [] }, //Esto excluye el through model, que traía por default la tabla "Drivers_Teams".
+        through: { attributes: [] }, 
       }, //Devuelve un array de objetos, si se quiere mostrar solo como un objeto, hay que hacer un map.
     });
  */
@@ -37,19 +32,13 @@ const getCampaignByName = async function (name) {
         },
       },
       /* include: {
-        model: Teams,
+        model: Categories,
         attributes: ["name"],
         through: { attributes: [] },
       }, */
     });
-    /* const dataBaseDrivers = cleanArrayDB(rawArrayDB);
-    const rawArrayApi = (await axios.get(`http://localhost:5000/drivers`)).data;
-    const driversApi = cleanArrayApi(rawArrayApi);
-    const filteredApi = driversApi.filter((driver) => {
-      return driver.name.toLowerCase().includes(name.toLowerCase()); // Busqueda inexacta
-    }); */
-    if (/* filteredApi.length > 0 ||  */ rawArrayDB.length > 0)
-      return rawArrayDB;
+
+    if (rawArrayDB.length > 0) return rawArrayDB;
     else throw new Error("Campaign name not found");
   }
 };
