925144105c00be103714c73e6e78b95ec0126c9c
CLiotta <camilo.liotta999@gmail.com>
2023-09-29 19:03:50 -0300

    feat/Footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import {Detail} from "./Components/Detail/Detail"
 import { DetailCampain } from './Components/Detail/Detail_campain'
 import { About } from './Components/About/About'
 import { Nav } from './Components/Nav/Nav'
+import Footer from './Components/Footer/Footer'
 import './App.css'
 import CreateCampaign from './Components/createCampaign/CreateCampaign'
 
@@ -19,6 +20,7 @@ function App() {
         <Route path='/create/campaign' element={<CreateCampaign/>}/>
         <Route path='/detail/camp' element={<DetailCampain/>}/>
       </Routes>
+      <Footer/>
     </>
   )
 }
