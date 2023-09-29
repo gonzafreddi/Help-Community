ad9890d3368ab3f6fe30605063adf809b665b51a
CLiotta <camilo.liotta999@gmail.com>
2023-09-29 17:28:53 -0300

    feat/About agregado

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,8 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './Components/Home/Home'
 import {Detail} from "./Components/Detail/Detail"
 import { DetailCampain } from './Components/Detail/Detail_campain'
-import './App.css'
+import { About } from './Components/About/About'
 import { Nav } from './Components/Nav/Nav'
+import './App.css'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/detail/:id' element={<Detail/>}/>
+        <Route path='/about' element={<About/>}/>
         <Route path='/detail/camp' element={<DetailCampain/>}/>
       </Routes>
     </>
