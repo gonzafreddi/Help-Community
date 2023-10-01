765cd5fd8e39d785f25e63a4d8c1faad47e6c35e
JuanPGalli <jpgallir@gmail.com>
2023-10-01 17:22:02 -0300

    feat(api/product): add route postProduct on /product. Fix getAllProducts.

---
+++ Api/helpers/productHelper.js
@@ -0,0 +1,37 @@
+const cleanArrayProductDB = (productsDB) =>
+  productsDB.map((product) => {
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      description: product.description,
+      image: product.image,
+      category: product.category,
+      /* donor: stringAll(product.ongDonorId), */
+      created: true,
+    };
+  });
+
+const cleanArrayProductApi = (productsApi) =>
+  productsApi.map((product) => {
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      description: product.description,
+      image: product.image,
+      category: product.category,
+      created: false,
+    };
+  });
+
+const stringAll = (Donors) => {
+  const teamsNames = Donors.map((donor) => donor.name);
+  return teamsNames.join(", ");
+};
+
+module.exports = {
+  cleanArrayProductApi,
+  cleanArrayProductDB,
+  stringAll,
+};
