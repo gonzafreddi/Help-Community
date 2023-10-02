1b57c36150cea91ab900cf12fa88ff8d34d4d176
LCCaruso <lucas.caruso27@gmail.com>
2023-10-02 11:44:56 -0300

    modificado filtros, revisarlos, cards agregado state y categoria

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 import style from "./Card.module.css";
 
 export const Card = (props) => {
-  const { nombreOng, nombre, descripcion, imagen, fechaInicio, fechaFin, objetivo, estado, categoria } = props;
+  const { nombreOng, nombre, descripcion, imagen, fechaInicio, fechaFin, objetivo, provincia, categoria } = props;
 
   return (
     <div className={style.contenedor}>
         <div className={style.contenedorImagen}>
-          <p className={style.ong}>{nombreOng}</p>
+          <p className={style.ong}>{nombreOng.toUpperCase()}</p>
           <img className={style.imagen} src="https://img.freepik.com/foto-gratis/usted-puede-ayudar-dar-concepto-donacion-bienestar_53876-121039.jpg?w=2000" alt={nombre} />
         </div>
       <p className={style.nombre}>{nombre}</p>
-      <p className={style.descripcion}>{descripcion}</p>
+      <p className={style.descripcion}>{descripcion}.</p>
+      <p className={style.provincia}>{provincia.toUpperCase()}</p>
+      <p className={style.categoria}>{categoria.toUpperCase()}</p>
       <p className={style.recaudado}>Objetivo: </p>
       {/* <p className={style.objetivo}>$8.560 <span className={style.de}>de</span> ${objetivo}</p> */}
       <p className={style.objetivo}>$ {objetivo}</p>
