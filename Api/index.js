d10e52044b2b447feec8a7d4eda0da447717dfb1
gonzalo freddi <freddigonzalo17@gmail.com>
2023-10-03 15:41:14 -0300

    add new routes

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,9 @@
 const server = require("./src/app.js");
+const { getAllCampaign } = require("./src/controllers/campaignController.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ alter: true }).then(() => {
+
+conn.sync({ alter: false }).then(() => {
   server.listen(3001, () => {
     console.log("Port listening at 3001");
   });
