3b900a60b89abbef9b42ee97b8aba4e6715d734b
ivan <ivanmantello@gmail.com>
2023-10-03 16:51:39 -0300

    cambios filtros

---
+++ Client/src/Components/FilterProducts/FilterProducts.jsx
@@ -0,0 +1,35 @@
+// import { filterByCateg } from '../../redux/actions/action';
+// import React, { useEffect, useState } from 'react';
+// import style from './FilterProducts.module.css';
+
+// const FilterProducts = ({ categ }) => {
+
+//     const [selectedCateg, setSelectedCateg] = useState(""); // Estado local 
+
+
+//     function handleFilterCateg(e) {
+//         const selectedValue = e.target.value;
+//         setSelectedCateg(selectedValue);
+//         dispatch(filterByCateg(selectedValue))
+//         }       
+    
+
+
+//     return (
+//         <div className={style.filterBarContainer}>
+//             <select className={style.filters} value={selectedCateg} onChange={handleFilterCateg}>
+//                 <option className={style.italic} value="" disabled>Filtrar por provincia</option>
+//                 <option className={style.casillero} value="Todos">Todas las provincias</option>
+//                 {categ.map((category) => (
+//                     <option className={style.opciones}  value={category.name}>
+//                         {category.name}
+//                     </option>
+//                 ))}
+//             </select>
+
+
+//         </div>
+//     );
+// };
+
+// export default FilterProducts;
