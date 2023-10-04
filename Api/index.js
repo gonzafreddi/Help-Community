3b29503424202877c9dc3c10257d33be45738d6b
JuanPGalli <jpgallir@gmail.com>
2023-10-04 17:19:04 -0300

    alter true

--- Api/index.js
+++ Api/index.js
@@ -2,9 +2,7 @@ const server = require("./src/app.js");
 //const { getAllCampaign } = require("./src/controllers/campaignController.js");
 const { conn } = require("./src/db.js");
 
-
-
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(3001, () => {
     console.log("Port listening at 3001");
   });
