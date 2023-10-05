d6cf6406a54bd08737f7209152890613628312e4
ivan <ivanmantello@gmail.com>
2023-10-05 18:22:51 -0300

    deje afuera la libreria ScrollReveal porque no era constante en la carga de cards

--- Client/src/Components/Products/Products.jsx
+++ Client/src/Components/Products/Products.jsx
@@ -4,194 +4,195 @@ import { getProduct, getCateg } from "../../redux/actions/action";
 import { Product } from "../Product/Product";
 import style from "./Products.module.css"
 import FilterProducts from '../FilterProducts/FilterProducts';
-import ScrollReveal from "scrollreveal";
-// import Pagination from "../Pagination/Pagination";
+// import ScrollReveal from "scrollreveal";
+
 
 
 
 export const Products = () => {
+
   const dispatch = useDispatch();
-  const [loadedCards, setLoadedCards] = useState(30);
-  const [isScrollRevealEnabled, setIsScrollRevealEnabled] = useState(true); // Controla si se debe aplicar ScrollReveal
+  const [loadedCards, setLoadedCards] = useState(30); // Inicialmente cargamos 30 tarjetas
+  const [cardsToLoad, setCardsToLoad] = useState(30); // Cantidad de tarjetas a cargar adicionalmente
+  
 
-  useEffect(() => {
-    dispatch(getProduct(loadedCards));
-    dispatch(getCateg());
-  }, [dispatch, loadedCards]);
 
-  const categ = useSelector((state) => state.categ);
-  const products = useSelector((state) => state.products);
+  
+useEffect(() => {
+  dispatch(getProduct(loadedCards));
+  dispatch(getCateg());
+}, [dispatch, loadedCards]);
 
-  const handleLoadMore = () => {
-    const totalCards = 100; // Supongamos 100 tarjetas en total
-    const remainingCards = totalCards - loadedCards;
-    const nextLoad = Math.min(30, remainingCards);
 
-    dispatch(getProduct(nextLoad, loadedCards));
-    setLoadedCards(loadedCards + nextLoad);
+  
 
-    // Deshabilitar ScrollReveal temporalmente al cargar más tarjetas
-    setIsScrollRevealEnabled(false);
+const categ = useSelector(state => state.categ);
 
-    // Espera un breve momento antes de volver a habilitar ScrollReveal
-    setTimeout(() => {
-      setIsScrollRevealEnabled(true);
-    }, 100);
-  };
+const products = useSelector((state) => state.products);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (
-        window.innerHeight + window.scrollY >=
-        document.body.offsetHeight - 200
-      ) {
-        handleLoadMore();
-      }
-    };
 
-    window.addEventListener("scroll", handleScroll);
+      useEffect(() => {
+        const handleScroll = () => {
+          if (
+            window.innerHeight + window.scrollY >=
+            document.body.offsetHeight - 200
+          ) {
+            // El usuario ha llegado al final de la página
+            handleLoadMore();
+          }
+        };
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+        window.addEventListener("scroll", handleScroll);
 
-  useEffect(() => {
-    if (isScrollRevealEnabled) {
-      initializeScrollReveal();
-    }
-  }, [isScrollRevealEnabled]);
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
 
-  const initializeScrollReveal = () => {
-    ScrollReveal().reveal('.card', {
-      duration: 800,
-      distance: '20px',
-      origin: 'bottom',
-      easing: 'ease-out',
-      reset: false,
-    });
-  };
 
-  return (
-    <div>
-      <FilterProducts categ={categ} />
+      const handleLoadMore = () => {
+        const totalCards = 100; // Supongamos 100 tarjetas en total
+        const remainingCards = totalCards - loadedCards;
+        const nextLoad = Math.min(cardsToLoad, remainingCards);
+      
+        dispatch(getProduct(nextLoad, loadedCards));
+        setLoadedCards(loadedCards + nextLoad);
+        setCardsToLoad(nextLoad);
 
-      <div className={style.cardsContainer}>
+        // initializeScrollReveal();
+        
+      };
+
+
+      // const initializeScrollReveal = () => {
+      //   ScrollReveal().reveal(".card", {
+      //     reset: true,
+      //     distance: "60px",
+      //     duration: 3000,
+
+      //   });
+      // };
+
+
+
+
+    return (
+      <div>
+        <FilterProducts categ={categ}/>
+
+        <div className={style.cardsContainer}>
         {products?.map((producto) => {
-          return (
-            <div
-              className={`card ${style.card}`}
-              key={producto.id}
-            >
-              <Product
-                nombre={producto.title}
-                descripcion={producto.description}
-                imagen={producto.images[0]}
-                precio={producto.price}
-                categoria={producto.category}
-              />
-            </div>
-          );
+          //  const cardClass = `${style.carlos} ${style.visible}`;
+
+        return  (
+          <div className={`${style.carlos} ${style.visible}`} key={producto.id}>
+                  <Product
+                  // key={producto.id}
+                  nombre={producto.title}
+                  descripcion={producto.description}
+                  imagen={producto.images[0]}
+                  precio={producto.price}
+                  categoria={producto.category}
+                />
+                </div>
+        );
         })}
       </div>
     </div>
-  );
-};
+    )
+  }
+
+  export default Products;
 
 
 
 
-
+  
 // export const Products = () => {
-
 //   const dispatch = useDispatch();
-//   const [loadedCards, setLoadedCards] = useState(30); // Inicialmente cargamos 30 tarjetas
-//   const [cardsToLoad, setCardsToLoad] = useState(30); // Cantidad de tarjetas a cargar adicionalmente
-  
+//   const [loadedCards, setLoadedCards] = useState(30);
+//   const [isScrollRevealEnabled, setIsScrollRevealEnabled] = useState(true); // Controla si se debe aplicar ScrollReveal
 
+//   useEffect(() => {
+//     dispatch(getProduct(loadedCards));
+//     dispatch(getCateg());
+//   }, [dispatch, loadedCards]);
 
-  
-// useEffect(() => {
-//   dispatch(getProduct(loadedCards));
-//   dispatch(getCateg());
-// }, [dispatch, loadedCards]);
+//   const categ = useSelector((state) => state.categ);
+//   const products = useSelector((state) => state.products);
 
+//   const handleLoadMore = () => {
+//     const totalCards = 100; // Supongamos 100 tarjetas en total
+//     const remainingCards = totalCards - loadedCards;
+//     const nextLoad = Math.min(30, remainingCards);
 
-  
+//     dispatch(getProduct(nextLoad, loadedCards));
+//     setLoadedCards(loadedCards + nextLoad);
 
-// const categ = useSelector(state => state.categ);
+//     // Deshabilitar ScrollReveal temporalmente al cargar más tarjetas
+//     setIsScrollRevealEnabled(false);
 
-// const products = useSelector((state) => state.products);
+//     // Espera un breve momento antes de volver a habilitar ScrollReveal
+//     setTimeout(() => {
+//       setIsScrollRevealEnabled(true);
+//     }, 100);
+//   };
 
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (
+//         window.innerHeight + window.scrollY >=
+//         document.body.offsetHeight - 200
+//       ) {
+//         handleLoadMore();
+//       }
+//     };
 
-//       useEffect(() => {
-//         const handleScroll = () => {
-//           if (
-//             window.innerHeight + window.scrollY >=
-//             document.body.offsetHeight - 200
-//           ) {
-//             // El usuario ha llegado al final de la página
-//             handleLoadMore();
-//           }
-//         };
+//     window.addEventListener("scroll", handleScroll);
 
-//         window.addEventListener("scroll", handleScroll);
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
 
-//         return () => {
-//           window.removeEventListener("scroll", handleScroll);
-//         };
-//       }, []);
+//   useEffect(() => {
+//     if (isScrollRevealEnabled) {
+//       initializeScrollReveal();
+//     }
+//   }, [isScrollRevealEnabled]);
 
+//   const initializeScrollReveal = () => {
+//     ScrollReveal().reveal('.card', {
+//       duration: 800,
+//       distance: '20px',
+//       origin: 'bottom',
+//       easing: 'ease-out',
+//       reset: false,
+//     });
+//   };
 
-//       const handleLoadMore = () => {
-//         const totalCards = 100; // Supongamos 100 tarjetas en total
-//         const remainingCards = totalCards - loadedCards;
-//         const nextLoad = Math.min(cardsToLoad, remainingCards);
-      
-//         dispatch(getProduct(nextLoad, loadedCards));
-//         setLoadedCards(loadedCards + nextLoad);
-//         setCardsToLoad(nextLoad);
+//   return (
+//     <div>
+//       <FilterProducts categ={categ} />
 
-//         initializeScrollReveal();
-        
-//       };
-
-
-//       const initializeScrollReveal = () => {
-//         ScrollReveal().reveal(".card", {
-//           reset: true,
-//           distance: "60px",
-//           duration: 3000,
-
-//         });
-//       };
-
-
-
-
-//     return (
-//       <div>
-//         <FilterProducts categ={categ}/>
-
-//         <div className={style.cardsContainer}>
+//       <div className={style.cardsContainer}>
 //         {products?.map((producto) => {
-
-//         return  (
-//           <div class="card" key={producto.id}>
-//                   <Product
-//                   // key={producto.id}
-//                   nombre={producto.title}
-//                   descripcion={producto.description}
-//                   imagen={producto.images[0]}
-//                   precio={producto.price}
-//                   categoria={producto.category}
-//                 />
-//                 </div>
-//         );
+//           return (
+//             <div
+//               className={`card ${style.card}`}
+//               key={producto.id}
+//             >
+//               <Product
+//                 nombre={producto.title}
+//                 descripcion={producto.description}
+//                 imagen={producto.images[0]}
+//                 precio={producto.price}
+//                 categoria={producto.category}
+//               />
+//             </div>
+//           );
 //         })}
 //       </div>
 //     </div>
-//     )
-//   }
-
-//   export default Products;
+//   );
+// };
