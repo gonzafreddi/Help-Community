87b7246fd49392397c71b9eab71b6b1435726c4f
ivan <ivanmantello@gmail.com>
2023-10-09 13:52:28 -0300

    Botones de prefio  al estilo selector/opciones

--- Client/src/Components/FilterProducts/FilterProducts.jsx
+++ Client/src/Components/FilterProducts/FilterProducts.jsx
@@ -3,32 +3,58 @@ import React, { useEffect, useState } from 'react';
 import style from './FilterProducts.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 
-const FilterProducts = ({ categ }) => {
+// const FilterProducts = ({ categ }) => {
 
-    const dispatch = useDispatch();
-    const [selectedCateg, setSelectedCateg] = useState("");
+//     const dispatch = useDispatch();
+//     const [selectedCateg, setSelectedCateg] = useState("");
 
 
-    function handleFilterCateg(e) {
-        const selectedValue = e.target.value;
-        setSelectedCateg(selectedValue);
-        dispatch(filterByCateg(selectedValue))
-        }       
+    // function handleFilterCateg(e) {
+    //     const selectedValue = e.target.value;
+    //     setSelectedCateg(selectedValue);
+    //     dispatch(filterByCateg(selectedValue))
+    //     }       
     
-    const ordenPrecio = (event) => {
-        dispatch(productOrdenPrecio(event.target.name))
-        }
+    // const ordenPrecio = (event) => {
+    //     dispatch(productOrdenPrecio(event.target.name))
+    //     }
 
-    const filtrosPrecio = (event) => {
-        dispatch(productsFiltrosPrecio(event.target.name))
-        }
+    // const filtrosPrecio = (event) => {
+    //     dispatch(productsFiltrosPrecio(event.target.name))
+    //     }
 
-    const reset = (event) => {
-        dispatch(resetProducts(event.target.name))
-    }
+    // const reset = (event) => {
+    //     dispatch(resetProducts(event.target.name))
+    // }
 
+    const FilterProducts = ({ categ }) => {
+        const dispatch = useDispatch();
+        const [selectedCateg, setSelectedCateg] = useState("");
+        const [selectedPriceFilter, setSelectedPriceFilter] = useState("");
+        const [selectedOrderFilter, setSelectedOrderFilter] = useState("");
     
-        // console.log("categorias: ", categ)
+        function handleFilterCateg(e) {
+            const selectedValue = e.target.value;
+            setSelectedCateg(selectedValue);
+            dispatch(filterByCateg(selectedValue));
+        }
+    
+        const handlePriceFilterChange = (e) => {
+            setSelectedPriceFilter(e.target.value);
+            dispatch(productsFiltrosPrecio(e.target.value));
+        }
+    
+        const handleOrderFilterChange = (e) => {
+            setSelectedOrderFilter(e.target.value);
+            dispatch(productOrdenPrecio(e.target.value));
+        }
+    
+        const reset = () => {
+            setSelectedPriceFilter(""); // Reinicia los selectores de precio y orden
+            setSelectedOrderFilter("");
+            dispatch(resetProducts());
+        }
+    
     
     const capitalizeFirstLetter = (str) => {            // Para poner la primera letra de la lista de categs en mayuscula
         if (str.length === 0) {
@@ -51,18 +77,20 @@ const FilterProducts = ({ categ }) => {
                 ))}
             </select>
             
-            <select className={style.filters}>
-            <option className={style.casillero} name="precioMenor" onClick={ordenPrecio}>Menor precio</option>
-            <option className={style.casillero} name="precioMayor" onClick={ordenPrecio}>Mayor precio</option>
+            <select className={style.filters} value={selectedPriceFilter} onChange={handlePriceFilterChange}>
+                <option className={style.italic} value="" disabled>Filtrar por precio</option>
+                <option className={style.casillero} value="menor100">Menor a $100</option>
+                <option className={style.casillero} value="menor500">Menor a $500</option>
+                <option className={style.casillero} value="menor1000">Menor a $1000</option>
+                <option className={style.casillero} value="mayor1000">Mayor a $1000</option>
             </select>
 
-            <select className={style.filters}>         
-            <option className={style.casillero} name="menor100" onClick={filtrosPrecio}>Menor a $100</option>
-            <option className={style.casillero} name="menor500" onClick={filtrosPrecio}>Menor a $500</option>
-            <option className={style.casillero} name="menor1000" onClick={filtrosPrecio}>Menor a $1000</option>
-            <option className={style.casillero} name="mayor1000" onClick={filtrosPrecio}>Mayor a $1000</option>
-            </select>  
-            
+            <select className={style.filters} value={selectedOrderFilter} onChange={handleOrderFilterChange}>
+                <option className={style.italic} value="" disabled>Ordenar por precio</option>
+                <option className={style.casillero} value="precioMenor">Menor precio</option>
+                <option className={style.casillero} value="precioMayor">Mayor precio</option>
+            </select>
+
             <button className={style.reset} onClick={reset}>RESET FILTERS</button>
         </div>
     );
