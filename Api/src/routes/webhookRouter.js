22bed7758900325f6ce7eac438f1acfeffa5c3d9
gonzalo freddi <freddigonzalo17@gmail.com>
2023-10-11 17:12:41 -0300

    marge in to develop

--- Api/src/routes/webhookRouter.js
+++ Api/src/routes/webhookRouter.js
@@ -3,7 +3,7 @@ const webhooRouter = express.Router();
 
 const receiveWebhook = require("../controllers/mercadoPago/webhook.js");
 
-
+ 
 
 webhooRouter.post("/webhook", receiveWebhook)// ? router.post por si el usuario cierra mercado pago sin volver a nuestra app
 
