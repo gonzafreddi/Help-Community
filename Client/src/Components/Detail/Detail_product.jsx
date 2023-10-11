d642d35b4c44f7d1ac8208ac7b30a171e727209f
LCCaruso <lucas.caruso27@gmail.com>
2023-10-10 22:51:03 -0300

    estructura ok

--- Client/src/Components/Detail/Detail_product.jsx
+++ Client/src/Components/Detail/Detail_product.jsx
@@ -14,6 +14,7 @@ export const DetailProduct = () => {
     const dispatch = useDispatch();
     const { name } = useParams();
     const [loading, setLoading] = useState(true);
+    const [isReviewPopupOpen, setReviewPopupOpen] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -34,6 +35,21 @@ export const DetailProduct = () => {
         console.log(detailProduct)
         dispatch(createOrder(detailProduct))
     }
+
+    const openReviewPopup = () => {
+        setReviewPopupOpen(true);
+      };
+      
+      const closeReviewPopup = () => {
+        setReviewPopupOpen(false);
+      };
+
+      const submitReview = () => {
+    // Lógica para procesar la revisión aquí
+
+    // Cierra el popup
+        closeReviewPopup();
+        };
       
 
     return (
@@ -66,11 +82,59 @@ export const DetailProduct = () => {
                         </div>
                     </div>
                         <div className={style.reviewsCont}>
-                            <h2>Reviews</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, 
-                                enim asperiores tempore iste nam, possimus itaque facilis, suscipit 
+                            <div className={style.contTitulos}>
+                            <h2>Product Reviews</h2>
+                            <button className={style.añadir} onClick={openReviewPopup}>Añadir review</button>
+                            </div>
+                            {isReviewPopupOpen && (
+                            <div className={style.modalBackground}>
+                                <div className={style.reviewPopup}>
+                                {/* Contenido del popup aquí */}
+                                <h2>¿Que opinas sobre este producto?</h2>
+                                <div className={style.imgReviewCont} >
+                                <img className={style.imgReview} src={product?.image} alt="" />
+                                </div>
+                                <div className={style.areaNombres}>
+                                <input className={style.nombreRev} type="text" placeholder="Nombre de Usuario" />
+                                <input className={style.puntajeRev} type="number" min="0" max="5" placeholder="Puntaje (0-5)" />
+                                </div>
+                                <textarea className={style.escribirRev} placeholder="Escribe tu opinión" />
+                                <div className={style.botonesReview}>
+                                <button className={style.enviar} onClick={submitReview}>Enviar</button>
+                                <button className={style.cancelar} onClick={closeReviewPopup}>Cancelar</button>
+                                </div>
+                                </div>
+                            </div>
+                            )}
+                            <div className={style.rev2}>
+                            <div className={style.review}>
+                                <h5>10/09/2023</h5>
+                                <h3>Pedro Gomez</h3>
+                                <h4>Puntaje: {product?.rating} / 5</h4>
+                                <p>
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit, suscipit 
                                 rem soluta velit officiis alias assumenda reprehenderit cum. Quibusdam, 
-                                nisi fugit. Ea.</p>
+                                nisi fugit. Ea."
+                                </p>
+                            </div>
+                            <div className={style.review}>
+                                <h5>24/08/2023</h5>
+                                <h3>Antonio Rivero</h3>
+                                <h4>Puntaje: {product?.rating} / 5</h4>
+                                <p>
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+                                </p>
+                            </div>
+                            <div className={style.review}>
+                                <h5>04/01/2023</h5>
+                                <h3>Gonzalo Gonzales</h3>
+                                <h4>Puntaje: {product?.rating} / 5</h4>
+                                <p>
+                                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, 
+                                enim asperiores tempore."
+                                </p>
+                            </div>
+                            </div>
                         </div>
                     </div>
                 )
