78b5394065b2b23cf65bc970150c3b21faecedde
JuanPGalli <jpgallir@gmail.com>
2023-10-12 02:33:17 -0300

    feat(Api/Buys): add route getAllBuys

--- Api/src/controllers/buysController.js
+++ Api/src/controllers/buysController.js
@@ -1,35 +1,34 @@
 const axios = require("axios");
-const { Campaign, Category, Buy, State } = require("../db");
+const { Campaign, Category, Buys, State } = require("../db");
 const { Op } = require("sequelize");
 
-
 const getAllbuys = async function () {
+  const allBuys = await Buys.findAll();
+  const parsedBuys = allBuys.map((buy) => {
+    const parsedProducts = JSON.parse(buy.products);
 
+    // Replace the "products" property with the parsed array
+    return {
+      ...buy.dataValues, // Preserve other properties
+      products: parsedProducts,
+    };
+  });
+
+  return parsedBuys;
 };
 
-
-const postbuys = async (
-    productId,
-    userId,
-    buyOut,
-    totalAmount,
-) => {
-  const newBuy = await Buy.create({
+/* const postbuys = async (productId, userId, buyOut, totalAmount) => {
+  const newBuy = await Buys.create({
     productId,
     userId,
     buyOut,
     totalAmount,
   });
-  
+
   return newBuy;
-};
+}; */
 
 module.exports = {
   getAllbuys,
-  postbuys,
+  /* postbuys, */
 };
-
-
-
-
-
