092500ffaa8907329e66bcfafc27cf2e8ced9f67
gonzalo freddi <freddigonzalo17@gmail.com>
2023-10-13 17:40:25 -0300

    (feature filter and pagination in AllBuys)

--- Api/src/controllers/mercadoPago/failure.js
+++ Api/src/controllers/mercadoPago/failure.js
@@ -2,6 +2,5 @@
 module.exports = (req,res)=> {
     console.log(req.query);
     // res.send('Pago rechazado')
-    window.alert("no se pudo concretar su compra")
     res.redirect('http://localhost:3000/products')
 };
