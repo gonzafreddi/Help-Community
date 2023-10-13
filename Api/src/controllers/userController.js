15c7e933b8e5dad0e4999c16799fcc29e52ca7bb
gonzalo freddi <freddigonzalo17@gmail.com>
2023-10-13 12:15:40 -0300

    resolve bug server

--- Api/src/controllers/userController.js
+++ Api/src/controllers/userController.js
@@ -44,7 +44,6 @@ const postUser = async (
   // userSuperadmin
 ) => {
   console.log(email);
-<<<<<<< HEAD
   try {
     const newUser = await User.findOrCreate({
       where: {
@@ -60,18 +59,6 @@ const postUser = async (
   } catch (error) {
     console.log(error.message)
   }
-=======
-  const [newUser, created] = await User.findOrCreate({
-    where: { email },
-    defaults: {
-      name,
-      image,
-      userState,
-      userAdmin,
-      userSuperadmin,
-    },
-  });
->>>>>>> 9b9713d7f4910e62c85511156d1bf23bf2157a3a
 
   return newUser;
 };
