352bf913ad981912809033a7e24edb761894439c
gonzalo freddi <freddigonzalo17@gmail.com>
2023-10-14 18:22:11 -0300

    buy in the cart shop

--- Api/src/controllers/mercadoPago/paymentController.js
+++ Api/src/controllers/mercadoPago/paymentController.js
@@ -11,21 +11,44 @@ const createOrder = async (req, res) => {
       "TEST-6850757546672488-100711-50177e634a355fb71db241599efdb0f3-1503945548",
   });
   // console.log(req.body[0].price)
-  console.log(req.body[0].image);
+  
   const pictureUrl = req.body[0].image;
+  const products = req.body[0]
+  console.log(products)
+  let items = [];
+  console.log(products.length)
+  if(products.length > 1){
 
+
+    for (const prod of products) {
+        const pictureUrl = prod.product.image;
+        const item = {
+          title: prod.product.name,
+          id: prod.product.id,
+          unit_price: prod.product.price,
+          currency_id: "ARS",
+          quantity:prod.product.quantity || 1,
+          picture_url: pictureUrl,
+        };
+        items.push(item);
+      }
+
+  }else{
+  items = [{
+    title: req.body[0].name,
+    id: req.body[0].id,
+    unit_price: req.body[0].price,
+    currency_id: "ARS",
+    quantity: req.body.quantity || 1,
+    userEmail: req.body[0].email,
+    picture_url: pictureUrl,
+  }]
+  }
+
+
+  console.log(items)
   const result = await mercadopago.preferences.create({
-    items: [
-      {
-        title: req.body[0].name,
-        id: req.body[0].id,
-        unit_price: req.body[0].price,
-        currency_id: "ARS",
-        quantity: req.body.quantity || 1,
-        userEmail: req.body[0].email,
-        picture_url: pictureUrl,
-      },
-    ],
+    items: items,
 
     // ? Darle el control a mercado pago...
     back_urls: {
@@ -33,8 +56,8 @@ const createOrder = async (req, res) => {
       failure: "http://localhost:3001/payment/failure",
       pending: "http://localhost:3001/payment/pending", //cuando el usuario no ha pagado
     },
-    notification_url: `https://mvrmrv5j-3001.brs.devtunnels.ms/payment/webhook?email=${req.body[0].email}`,
-    // notification_url:`https://pmcmwdd1-3001.brs.devtunnels.ms/payment/webhook?email=${req.body[0].email}`, // Puerto de gonzalo freddi si quieren probar cambien la url con su puerto https
+    // notification_url: `https://mvrmrv5j-3001.brs.devtunnels.ms/payment/webhook?email=${req.body[0].email}`,
+    notification_url:`https://pmcmwdd1-3001.brs.devtunnels.ms/payment/webhook?email=${req.body[0].email}`, // Puerto de gonzalo freddi si quieren probar cambien la url con su puerto https
   });
   // console.log(result.body);
   res.send(result.body);
